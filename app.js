0d2a1bcaaf70894e4626b1287598e4a4a4a71417
Alexander Drakon <drakon.a.v@gmail.com>
2023-12-04 15:56:50 +0300

    feat: initial structure

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
+const cookieParser = require('cookie-parser');
+const { celebrate, errors } = require('celebrate');
+const bodyParser = require('body-parser');
+
+require('dotenv').config();
+
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 100,
+  standardHeaders: true,
+  legacyHeaders: false,
+});
+
+const app = express();
+
+mongoose.connect(DB_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(helmet());
+app.use(limiter);
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+app.use(errors());
+
+app.listen(PORT);
