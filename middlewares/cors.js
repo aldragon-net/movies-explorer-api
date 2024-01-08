22c3374bf6d8bc15f452ba349b4914346636d907
Alexander Drakon <drakon.a.v@gmail.com>
2024-01-08 21:32:19 +0300

    feat: local-cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -7,10 +7,8 @@ module.exports.cors = (req, res, next) => {
   const { method } = req;
   const { origin } = req.headers;
   const requestHeaders = req.headers['access-control-request-headers'];
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Credentials', true);
-    res.header('Access-Control-Allow-Origin', origin);
-  }
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', origin); 
   if (method === 'OPTIONS') {
     res.status(STATUSES.OK);
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
